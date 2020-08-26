ca737fe8245b83a4e487850ee380ccb092f9c090
Albert Alcantara <aalcantara@gcs-systems.com>
2020-08-25 23:48:44 -0400

    Hice cambio en el HTML, CSS y JS

---
+++ js/archivo.js
@@ -0,0 +1,11 @@
+//Esto arroja el valor de una variable
+class Persona {
+nombre = null;
+edad = null;
+}
+
+var persona = new persona();
+persona.nombre = "albert";
+persona.edad = 20;
+
+console.log(persona.nombre + persona.edad);
